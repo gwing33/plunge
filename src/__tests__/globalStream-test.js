2a3eaee3cba7859757fc8eb62bdcd24c1b495472
Gerald Leenerts <gerald.leenerts@gmail.com>
2017-02-01 08:41:48 -0700

    Put things in better places, also explained how it works better.

--- src/__tests__/globalStream-test.js
+++ src/__tests__/globalStream-test.js
@@ -1,12 +1,21 @@
 import {TestScheduler} from 'rxjs';
 
-describe('Global Stream Tests', () => {
-  it('Should send a message without data', () => {
-    const rxTestScheduler = new TestScheduler((actual, expected) => {
-      expect(actual).toEqual(expected);
-    });
+const {globalStream} = require('../globalStream');
+const rxTestScheduler = new TestScheduler((actual, expected) => {
+  expect(actual).toEqual(expected);
+});
 
-    const {globalStream} = require('../globalStream');
+describe('Global Stream Tests', () => {
+  it('Should send some messages through globalStream', () => {
+    /* * *
+     * Our goal is to send 3 events into the globalStream (GS). In order to do
+     * that, we need to add an initinal observable that actually sends the data to GS.
+     *
+     * Mock Data --> TestObservable --> GS --> expected output
+     *
+     * Before we set up the TestObservable, we need to subscribe to the GS.
+     * Nothing will be kicked off until we run the `flush`.
+     */
 
     const main     = '--a--b--c--|';
     const expected = '-----b-----|';
@@ -20,15 +29,15 @@ describe('Global Stream Tests', () => {
       b: {action: "world", data: {}}
     };
 
-    // Subscribe to the global stream to start listening for values.
+    // Subscribe to the GS to start listening for values.
     rxTestScheduler.expectObservable(globalStream).toBe(expected, expectedVals);
 
-    // Redirect the values into the globalStream
+    // TestObservable that redirects data to the GS
     const result = rxTestScheduler.createHotObservable(main, mainVals).do(
       x => globalStream.next(x), e => globalStream.error(e), () => globalStream.complete()
     );
 
-    // Add test data to be able to be flushed, which will flow the data to the globalStream.
+    // Add TestObservable to the flush queue.
     rxTestScheduler.expectObservable(result).toBe(main, mainVals);
 
     // Flush the tests which will run the deep compare
